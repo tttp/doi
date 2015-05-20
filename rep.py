02496999004bbb8ba0492aae692990aca8d59cde
xavier <xavier@tttp.eu>
2015-05-20 07:59:41 +0200

    add more DG parsed

--- rep.py
+++ rep.py
@@ -6,15 +6,17 @@ meetings = []
 for data in json.loads(json_data):
   if len(data["participants"]) > 0:
     for participant in (data["participants"]):
-      meeting =  copy.deepcopy(data)
-      del meeting["participants"]
+      #meeting =  copy.deepcopy(data)
+      meeting =  copy.copy(data)
+#      del meeting["participants"]
       meeting["participant"]= participant[1]
       meeting["participant_id"]= participant[0]
+      print meeting
       meetings.append (meeting)
 
   else:
-    meeting["participant"]= ""
-    meeting["participant_id"]= ""
+    data["participant"]= ""
+    data["participant_id"]= ""
     del data["participants"]
     meetings.append (data)
 
