6cb32ab0e7e1663b753ba93e33d8f89b0af5f280
xavier <xavier@tttp.eu>
2015-06-04 23:48:13 +0200

    add gulp + lobbyist graph

--- data/update.sh
+++ data/update.sh
@@ -20,6 +20,7 @@ jq -r ".[]|select(.cancelled=false)|[.id,.ec_representative,.date,.subject,.part
 
 #nice -n19 jq 'map( del(.contact_more) | del(.networking)| del(.goals) | del (.activities) | del(.uri) | del(.code_of_conduct)| del(.contact_fax) | del(.status) | del(.head) | del(.head) | del (.contact_street) | del(.entity) | del(.info_members) | del(.contact_phone) | del(.created_at) | del(.legal) | del(.contact_post_code) )' representative.json > representative-light.json
 
+#curl -O http://api.lobbyfacts.eu/api/1/financial_data.csv > financial_data.csv
 
 csvjoin   --left -c id,representative representative.csv financial_data.csv  > representative-financial.csv
 csvcut representative-financial.csv -c id,contact_country,sub_category,acronym,name,main_category,cost_min,cost_max,cost_absolute > representative-finance-light.csv
