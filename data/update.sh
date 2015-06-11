04ba4f4f2edec3586e15715561be2dc8d047fb84
xavier <xavier@tttp.eu>
2015-06-11 11:59:14 +0200

    update with new meeting format

--- data/update.sh
+++ data/update.sh
@@ -9,15 +9,16 @@ jq -M ".[]|[.id,.name]|@csv" ../country.json >> country.csv
 #jq -r ".[]|[.id,.contact_country,.acronym,.name,.main_category,.sub_category]|@csv" representative.json >> representative.csv
 
 #debug version
-echo 'id,contact_country,sub_category,sub_category_title,acronym,name,main_category,main_category_title,code' >  representative.csv
-jq -r ".[]|[.id,.contact_country,.sub_category,.sub_category_title,.acronym,.name,.main_category,.main_category_title,.identification_code]|@csv" representative.json >> representative.csv
+echo 'id,contact_country,sub_category,sub_category_title,acronym,name,main_category,main_category_title,code,fte' >  representative.csv
+jq -r '.[]|select(.status=="active")|[.id,.contact_country,.sub_category,.sub_category_title,.acronym,.name,.main_category,.main_category_title,.identification_code,.members_fte]|@csv' representative.json >> representative.csv
 
 #meeting
 sed -i 's/T00:00:00//g' meeting-flat.json 
-echo 'id,host,date,title,guest,unregistered,guestid,cabinet' > meeting.csv 
+echo 'id,host,date,title,guest,unregistered,guestid,cabinet,nb' > meeting.csv 
 #jq -r ".[]|select(.cancelled=false)|[.id,.ec_representative,.date,.subject,.participants[0][1],.participants[0][0],.ec_org]|@csv" meeting-flat.json  >> meeting.csv
 #jq -r ".[]|select(.cancelled=false)|[.id,.ec_representative,.date,.subject,.participants[0][1],.unregistered,.participants[0][0],.ec_org]|@csv" meeting-flat.json  >> meeting.csv
-jq -r ".[]|select(.cancelled=false)|[.id,.ec_representative,.date,.subject,.participant,.unregistered,.participant_id,.ec_org]|@csv" meeting-flat.json  >> meeting.csv
+#jq -r '.[]|select(.cancelled=false)|select(.status=="active")|[.id,.ec_representative,.date,.subject,.participant,.unregistered,.participant_id,.ec_org]|@csv' meeting-flat.json  >> meeting.csv
+jq -r '.[]|[.id,.ec_representative,.date,.subject,.participant,.unregistered,.participant_id,.ec_org,.nb]|@csv' meeting-flat.json  >> meeting.csv
 #jq -r ".[]|select(.cancelled=false)|{id,ec_representative,date,subject,ec_org, guest: .participants[0][0],idguest:.participants[0][1]}" meeting.json > meeting-light.json
 
 #nice -n19 jq 'map( del(.contact_more) | del(.networking)| del(.goals) | del (.activities) | del(.uri) | del(.code_of_conduct)| del(.contact_fax) | del(.status) | del(.head) | del(.head) | del (.contact_street) | del(.entity) | del(.info_members) | del(.contact_phone) | del(.created_at) | del(.legal) | del(.contact_post_code) )' representative.json > representative-light.json
@@ -25,7 +26,7 @@ jq -r ".[]|select(.cancelled=false)|[.id,.ec_representative,.date,.subject,.part
 #curl -O http://api.lobbyfacts.eu/api/1/financial_data.csv > financial_data.csv
 
 csvjoin   --left -c id,representative representative.csv financial_data.csv  > representative-financial.csv
-csvcut representative-financial.csv -c id,contact_country,sub_category,acronym,name,main_category,cost_min,cost_max,cost_absolute,code > representative-finance-light.csv
+csvcut representative-financial.csv -c id,contact_country,sub_category,acronym,name,main_category,cost_min,cost_max,cost_absolute,fte,code > representative-finance-light.csv
 
 #curl -O http://api.lobbyfacts.eu/api/1/accreditation.csv > accreditation.csv
 echo 'id,accredited' > representative_count.csv
--- rep.py
+++ rep.py
@@ -3,49 +3,67 @@ import copy
 from collections import defaultdict
 
 
-def get_reps(ps):
+def get_reps(ps,type):
     participants=[]
-    for token in ps.split(', '):
+    for token in ps.split('), '):
         idx=token.find('(')
         idx2=token.find(')')
-        if 30<idx<=32:
-            try: int(token[:idx],16)
-            except: continue
-            #print 'asdf', idx, token
-            participants.append([token[:idx],token[idx+1:idx2]])
+        if (idx2 == -1):
+            idx2=10000 # take all the id
+#        if 30<idx<=32:
+#            try: int(token[:idx],16)
+#            except:
+#              print token
+#              continue
+        participants.append([token[:idx],token[idx+1:idx2],type])
     return participants
 
 
 json_data=open('meeting.json').read()
 meetings = []
 count=0
+inactive=0
+cancelled=0
 #counter = defaultdict(int)
 for data in json.loads(json_data):
+  if data["cancelled"]==True:
+    cancelled +=1
+    continue
   if data["status"]=="inactive":
+    inactive +=1
     continue
   count +=1
+  nbparticipants = 0 
   if len(data["participants"]) > 0:
-    ps = get_reps(data["participants"])
-
-    for participant in (ps):
+    ps = get_reps(data["participants"],"registered")
+    nbparticipants += 1+data["participants"].count(',')
+  if len(data["deregistered"]) > 0:
+    ps = get_reps(data["deregistered"],"deregistered")
+    nbparticipants += 1+data["deregistered"].count(',')
+  if hasattr(data, 'unregistered') and len(data["unregistered"]):
+    nbparticipants +=1
+    ps = ["",data["unregistered"],"unregistered"]
+    del meeting["unregistered"]
+  for participant in (ps):
 #      meeting =  copy.deepcopy(data)
-      meeting =  copy.copy(data)
-      
-      meeting["participant_id"]= participant[0]
-      meeting["participant"]= participant[1]
-      meetings.append (meeting)
+    meeting =  copy.copy(data)
+    meeting["nb"]= nbparticipants
+    meeting["participant_id"]= participant[1]
+    meeting["participant"]= participant[0]
+    meeting["status"]= participant[2]
+    del meeting["cancelled"]
+    del meeting["participants"]
+    del meeting["deregistered"]
+    del meeting["unregistered"]
+    meetings.append (meeting)
 #      counter[meeting["id"]] +=1
-      del meeting["participants"]
+    #del meeting["participants"]
 
     #raise SystemExit
 
-  else:
-    data["participant"]= ""
-    data["participant_id"]= ""
-    del data["participants"]
-    meetings.append (data)
-
-print count
+print "active:" + `count`
+print "inactive:"+`inactive`
+print "cancelled:"+`cancelled`
 print len(meetings)
 with open('meeting-flat.json', 'w') as fp:
     json.dump(meetings, fp)
