1e7d537007f30f47f9f774ab40be8157a5d24ae3
xavier <xavier@tttp.eu>
2015-06-14 18:43:40 +0200

    new source

--- data/update.sh
+++ data/update.sh
@@ -4,6 +4,18 @@
 echo 'id,name' > country.csv
 jq -M ".[]|[.id,.name]|@csv" ../country.json >> country.csv
 
+#dealing with meeting_participants.csv
+q 'select `meetings.id` as id,"registered" as status ,representative as guest,`meetings.ec_representative` as host,`meetings.ec_org` as cabinet,`meetings.date` as date,representative_id as guestid,`meetings.subject` as title,`meetings.unregistered` as unregistered, "0" as nb from meeting_participants.csv where `meetings.status` = "active" and unregistered="" ' -d, -H  -O > meeting_flat.csv
+
+q 'select `meetings.id` as id,"registered" as status ,representative as guest,`meetings.ec_representative` as host,`meetings.ec_org` as cabinet,`meetings.date` as date,representative_id as guestid,`meetings.subject` as title,`meetings.unregistered` as unregistered, "0" as nb from meeting_participants.csv where `meetings.status` = "active"' -d, -H   >> meeting_flat.csv
+
+q 'select `meetings.unregistered` as unregistered from meeting_participants.csv where unregistered!="" limit 10' -d, -H -O
+
+
+#dealing with meeting_deregistered.csv
+q 'select meeting.id as id,"dereg" as status ,guest.name as guest, meeting.ec_representative as host,meeting.ec_org as cabinet,meeting.date as date,identification_code as guestid,meeting.subject as title,meeting.unregistered as unregistered, "0" as nb  from  meeting.csv meeting join meeting_deregistered.csv guest on (meeting.id = guest.meeting) where guest.status="active"' -d, -H >> meeting_flat.csv
+
+
 #representative
 #echo 'id,contact_country,acronym,name,main_category,sub_category' >  representative.csv
 #jq -r ".[]|[.id,.contact_country,.acronym,.name,.main_category,.sub_category]|@csv" representative.json >> representative.csv
@@ -11,6 +23,8 @@ jq -M ".[]|[.id,.name]|@csv" ../country.json >> country.csv
 #debug version
 echo 'id,contact_country,sub_category,sub_category_title,acronym,name,main_category,main_category_title,code,fte' >  representative.csv
 jq -r '.[]|select(.status=="active")|[.id,.contact_country,.sub_category,.sub_category_title,.acronym,.name,.main_category,.main_category_title,.identification_code,.members_fte]|@csv' representative.json >> representative.csv
+
+exit
 
 #meeting
 sed -i 's/T00:00:00//g' meeting-flat.json 
@@ -29,8 +43,8 @@ csvjoin   --left -c id,representative representative.csv financial_data.csv  > r
 csvcut representative-financial.csv -c id,contact_country,sub_category,acronym,name,main_category,cost_min,cost_max,cost_absolute,fte,code > representative-finance-light.csv
 
 #curl -O http://api.lobbyfacts.eu/api/1/accreditation.csv > accreditation.csv
-echo 'id,accredited' > representative_count.csv
-#q "select representative_id,count(*) FROM accreditation.csv where status='active' group by representative_id"  -d, -H >> representative_count.csv;
+#echo 'id,accredited' > representative_count.csv
+q "select representative_id id,count(*) accredited FROM accreditation.csv where status='active' group by representative_id"  -d, -H -O >> representative_count.csv;
 
 csvjoin --left -c id,id  representative-finance-light.csv representative_count.csv  > r.csv
 
