4e93aaa9499184ba74fcaa670c58ac318c5dec81
xavier <xavier@tttp.eu>
2015-07-06 12:24:37 +0200

    update data

--- data/fetch.sh
+++ data/fetch.sh
@@ -1,6 +1,6 @@
 curl -O http://api.lobbyfacts.eu/api/1/meeting_deregistered.csv 
 curl -O http://api.lobbyfacts.eu/api/1/meeting_participants.csv 
 curl -O http://api.lobbyfacts.eu/api/1/meeting.csv 
-curl http://api.lobbyfacts.eu/api/1/financial_data.csv  > representative-financial.csv
+curl -O http://api.lobbyfacts.eu/api/1/financial_data.csv
 curl http://api.lobbyfacts.eu/api/1/country.csv > country-data.csv
 
