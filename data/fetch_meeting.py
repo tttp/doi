8abca8a03e78d0cb73a7279196ee27a533edfba2
xavier <github@tttp.eu>
2016-04-14 20:25:10 +0200

    update

--- data/fetch_meeting.py
+++ data/fetch_meeting.py
@@ -6,6 +6,7 @@ offset=0
 # Set the request URL
 url = 'http://api.lobbyfacts.eu/api/1/meeting?limit='+`limit`+'&offset='+`offset`
 u = urllib.urlopen(url)
+print u
 result = json.load(u) 
 total = result["count"]
 meeting = result["results"]
