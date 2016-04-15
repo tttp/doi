9ef5bb9d04a8d9d4e6f1acc65fe6fd15ed1db956
xavier <xavier@tttp.eu>
2016-04-15 14:58:56 +0200

    update representatives automatically

--- data/fetch.sh
+++ data/fetch.sh
@@ -5,4 +5,4 @@ curl -O http://api.lobbyfacts.eu/static/meeting.csv
 curl -O http://api.lobbyfacts.eu/api/1/financial_data.csv
 curl -O http://parltrack.euwiki.org/dumps/attendance.csv
 curl http://api.lobbyfacts.eu/api/1/country.csv > country-data.csv
-
+python fetch_representative.py
