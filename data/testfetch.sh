a80b52a2edf938f4043ccdf4b9546fdf251ff7d2
GChiara <girardelli.chiara@gmail.com>
2016-10-07 17:37:17 +0200

    added script for testing fetch

--- data/testfetch.sh
+++ data/testfetch.sh
@@ -2,5 +2,5 @@ export PYENV_ROOT="/home/xavier/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
-curl -O https://lobbyfacts.eu/transparency_meetings > meeting-test.csv
+curl https://lobbyfacts.eu/transparency_meetings > meeting-test.csv
 
