9d6007ceb79d42be91db0d172f225fe46f0417cb
GChiara <girardelli.chiara@gmail.com>
2016-10-07 17:38:32 +0200

    fixed fetch

--- data/fetch.sh
+++ data/fetch.sh
@@ -2,14 +2,13 @@ export PYENV_ROOT="/home/xavier/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
-curl -O https://lobbyfacts.eu/transparency_meetings > meeting2.csv
 curl http://stage.lobbyfacts.nestor.coop/transparency_csv > transparency.csv
 curl -O http://api.lobbyfacts.nestor.coop/api/1/meeting_deregistered.csv 
 curl -O http://api.lobbyfacts.nestor.coop/api/1/meeting_participants.csv 
 #curl -O http://api.lobbyfacts.eu/static/meeting.csv
 #curl -O http://api.lobbyfacts.nestor.coop/api/1/meeting.csv 
 #curl -O http://stage.lobbyfacts.nestor.coop/1/financial_data.csv
-curl -O https://lobbyfacts.eu/transparency_meetings > meeting.csv
+curl https://lobbyfacts.eu/transparency_meetings > meeting.csv
 curl -O http://parltrack.euwiki.org/dumps/attendance.csv
 #curl http://api.lobbyfacts.nestor.coop/1/country.csv > country-data.csv
 #curl http://api.lobbyfacts.eu/api/1/country.csv  > country-data.csv
