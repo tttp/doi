62872093deaf33c674ee14123e48d977c375f516
GChiara <girardelli.chiara@gmail.com>
2016-10-07 16:58:51 +0200

    updated meetings url in fetch

--- data/fetch.sh
+++ data/fetch.sh
@@ -8,6 +8,7 @@ curl -O http://api.lobbyfacts.nestor.coop/api/1/meeting_participants.csv
 #curl -O http://api.lobbyfacts.eu/static/meeting.csv
 #curl -O http://api.lobbyfacts.nestor.coop/api/1/meeting.csv 
 #curl -O http://stage.lobbyfacts.nestor.coop/1/financial_data.csv
+curl -O https://lobbyfacts.eu/transparency_meetings > meeting.csv
 curl -O http://parltrack.euwiki.org/dumps/attendance.csv
 #curl http://api.lobbyfacts.nestor.coop/1/country.csv > country-data.csv
 #curl http://api.lobbyfacts.eu/api/1/country.csv  > country-data.csv
